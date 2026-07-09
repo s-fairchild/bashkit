a266c6ee9f3994768151fbd60abf6a3c9a210a10
steven <steve51516@gmail.com>
2026-07-09 14:44:01 -0400

    Update temporary fatal function to fatal_tmp
    To prevent confusion as to which fatal is actually being used.

--- lib/logging.sh
+++ lib/logging.sh
@@ -11,11 +11,11 @@ declare -r __bashkit_lib_logging_sourced="true"
 #
 declare -r VENDOR_BASHKIT=${VENDOR_GIT_MODULES["bashkit"]:-..}
 
-declare -F fatal > /dev/null 2>&1 || fatal() { printf '%s\n' "${1:-}" >&2; exit 1; }
+fatal_tmp() { printf '%s\n' "${1:-}" >&2; exit 1; }
 
 if [ "${__bashkit_lib_logging_colors_sourced:=}" != "true" ]; then
     declare __lib_logging_colors="${VENDOR_BASHKIT}/lib/logging/colors.env"
-    [ -f "$__lib_logging_colors" ] || fatal "file not found: $__lib_logging_colors"
+    [ -f "$__lib_logging_colors" ] || fatal_tmp "file not found: $__lib_logging_colors"
     # shellcheck source=logging/colors.env
     . "$__lib_logging_colors"
     unset __lib_logging_colors
@@ -23,7 +23,7 @@ fi
 
 if [ "${__bashkit_lib_logging_symbols_sourced:=}" != "true" ]; then
     declare __lib_logging_symbols="${VENDOR_BASHKIT}/lib/logging/symbols.env"
-    [ -f "${__lib_logging_symbols}" ] || fatal "file not found: $__lib_logging_symbols"
+    [ -f "${__lib_logging_symbols}" ] || fatal_tmp "file not found: $__lib_logging_symbols"
     # shellcheck source=logging/symbols.env
     . "$__lib_logging_symbols"
     unset __lib_logging_symbols
@@ -31,8 +31,10 @@ fi
 
 if [ "${__bashkit_lib_logging_error_messages_sourced:=}" != "true" ]; then
     declare __lib_logging_error_messages="${VENDOR_BASHKIT}/lib/logging/error-messages.env"
-    [ -f "${__lib_logging_error_messages}" ] || fatal "file not found: $__lib_logging_error_messages"
+    [ -f "${__lib_logging_error_messages}" ] || fatal_tmp "file not found: $__lib_logging_error_messages"
     # shellcheck source=logging/error-messages.env
     . "$__lib_logging_error_messages"
     unset __lib_logging_error_messages
 fi
+
+unset -f fatal_tmp
